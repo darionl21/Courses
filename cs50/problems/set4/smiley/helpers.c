7ff49b98bec50de291395f3a14823ab8749ac34a
dreig-dawi <dreigbuades@cifpfbmoll.eu>
2023-12-19 11:54:35 +0100

    Smiley problem Switching the colors of the smiley

---
+++ cs50/problems/set4/smiley/helpers.c
@@ -0,0 +1,21 @@
+#include "helpers.h"
+#include "cs50.h"
+#include "stdio.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Change all black pixels to a color of your choosing
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtGreen == 0x00)
+            {
+                image[i][j].rgbtBlue = 0x00;
+                image[i][j].rgbtGreen = 0xff;
+                image[i][j].rgbtRed = 0xff;
+            }
+        }
+    }
+}
